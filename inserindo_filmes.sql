e50e76683e43588232cc55120a93e76b076003b3
CristianPessatti <criss.p.a@hotmail.com>
2021-04-02 16:51:44 -0300

    arrumando identacao

--- inserindo_filmes.sql
+++ inserindo_filmes.sql
@@ -3,7 +3,7 @@ USE locadora;
 SELECT * FROM filmes;
 
 INSERT INTO filmes VALUES (6, 'A forma d\'agua', 'romance', '02:03:00', 'DISPONIVEL', 8.5, NULL),
-						  (7, 'Green Book: O guia', 'drama', '02:10:00', 'DISPONIVEL', 6.7, NULL),
+                          (7, 'Green Book: O guia', 'drama', '02:10:00', 'DISPONIVEL', 6.7, NULL),
                           (8, 'O gênio indomável', 'drama', '02:07:00', 'DISPONIVEL', 8.2, NULL),
                           (9, 'Patch Adams', 'drama', '01:55:00', 'DISPONIVEL', 7.2, NULL),
                           (10, 'Escritores da Liberdade', 'drama','02:03:00', 'DISPONIVEL', 7.8, NULL); 
